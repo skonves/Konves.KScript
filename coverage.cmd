d133f5fe13f87d4e044c25b8531a18ddb884a5a7
Steve Konves <stephen.konves@gmail.com>
2016-09-03 12:04:20 -0700

    maybe this way?

--- coverage.cmd
+++ coverage.cmd
@@ -1,5 +1,5 @@
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
 
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"vstest.console.exe" -filter:"+[*]* -[*.*Tests]* -[*.Parsing.*]*" -targetargs:".\tests\Konves.KScript.UnitTests\bin\Release\Konves.KScript.UnitTests.dll" -register:user
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"vstest.console.exe" -filter:"+[*]* -[*.*Tests|*.g.cs]*" -targetargs:".\tests\Konves.KScript.UnitTests\bin\Release\Konves.KScript.UnitTests.dll" -register:user
 .\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
