1dd0ffbd5dffc3ef72f38a1c9f4f2c76e1ac74cf
Steve Konves <stephen.konves@gmail.com>
2016-09-03 19:31:31 -0700

    Add tests

--- tests/Konves.KScript.UnitTests/InExpressionTestFixture.cs
+++ tests/Konves.KScript.UnitTests/InExpressionTestFixture.cs
@@ -2,6 +2,7 @@
 using Konves.KScript.Expressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Konves.KScript.UnitTests
 {
@@ -12,16 +13,38 @@ namespace Konves.KScript.UnitTests
 		[TestMethod]
 		public void TestEvaluate()
 		{
+			string stringValue = "string value";
+			decimal decimalValue = 5m;
+			DateTime dateValue = DateTime.Parse("2015-1-1");
+			bool boolValue = true;
+
+			IReadOnlyCollection<object> list = new List<object> { stringValue, decimalValue, dateValue, boolValue };
+
+			DoTestEvaluate(null, list, false);
+			DoTestEvaluate(stringValue, list, true);
+			DoTestEvaluate(decimalValue, list, true);
+			DoTestEvaluate(dateValue, list, true);
+			DoTestEvaluate(boolValue, list, true);
+
+			DoTestEvaluate("not found", list, false);
+			DoTestEvaluate(1234m, list, false);
+			DoTestEvaluate(DateTime.Parse("2000-1-1"), list, false);
+			DoTestEvaluate(!boolValue, list, false);
 		}
 
-		private void DoTestEvaluate(bool expected)
+		private void DoTestEvaluate(object value, IReadOnlyCollection<object> list, bool expected)
 		{
 			// Arrange
-			//IExpression expression = new InExpression()
+			Literal literal = new Literal(value, LiteralType.Value);
+			IReadOnlyCollection<Literal> literals = list.Select(item => new Literal(item, LiteralType.Value)).ToList();
+
+			IExpression expression = new InExpression(literal, literals);
 
 			// Act
+			bool result = expression.Evaluate(null);
 
 			// Assert
+			Assert.AreEqual(expected, result);
 		}
 	}
 }
